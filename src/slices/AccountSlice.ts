244c31999e558550ab04c24a32ed9bab4151d638
itdeveloper72469 <itdeveloper72469@gmail.com>
2021-12-27 22:59:33 +0800

    This is a project of forking Olympus DAO to Polygon

---
+++ src/slices/AccountSlice.ts
@@ -0,0 +1,454 @@
+import { BigNumber, BigNumberish, ethers } from "ethers";
+import { addresses } from "../constants";
+import { abi as ierc20Abi } from "../abi/IERC20.json";
+import { abi as sOHMv2 } from "../abi/sOhmv2.json";
+import { abi as fuseProxy } from "../abi/FuseProxy.json";
+import { abi as wsOHM } from "../abi/wsOHM.json";
+import { abi as fiatDAO } from "../abi/FiatDAOContract.json";
+
+import { setAll, handleContractError } from "../helpers";
+
+import { createAsyncThunk, createSelector, createSlice } from "@reduxjs/toolkit";
+import { RootState } from "src/store";
+import { IBaseAddressAsyncThunk, ICalcUserBondDetailsAsyncThunk } from "./interfaces";
+import {
+  FiatDAOContract,
+  FuseProxy,
+  IERC20,
+  IERC20__factory,
+  SOhmv2,
+  WsOHM,
+  OlympusStakingv2__factory,
+} from "src/typechain";
+import { GOHM__factory } from "src/typechain/factories/GOHM__factory";
+
+export const getBalances = createAsyncThunk(
+  "account/getBalances",
+  async ({ address, networkID, provider }: IBaseAddressAsyncThunk) => {
+    let gOhmBalance = BigNumber.from("0");
+    let gOhmBalAsSohmBal = BigNumber.from("0");
+    let ohmBalance = BigNumber.from("0");
+    let sohmBalance = BigNumber.from("0");
+    let ohmV2Balance = BigNumber.from("0");
+    let sohmV2Balance = BigNumber.from("0");
+    let wsohmBalance = BigNumber.from("0");
+    let poolBalance = BigNumber.from("0");
+    let fsohmBalance = BigNumber.from(0);
+    let fgohmBalance = BigNumber.from(0);
+    let fgOHMAsfsOHMBalance = BigNumber.from(0);
+    let fiatDaowsohmBalance = BigNumber.from("0");
+    try {
+      const gOhmContract = GOHM__factory.connect(addresses[networkID].GOHM_ADDRESS, provider);
+      gOhmBalance = await gOhmContract.balanceOf(address);
+      gOhmBalAsSohmBal = await gOhmContract.balanceFrom(gOhmBalance.toString());
+    } catch (e) {
+      handleContractError(e);
+    }
+    try {
+      const wsohmContract = new ethers.Contract(addresses[networkID].WSOHM_ADDRESS as string, wsOHM, provider) as WsOHM;
+      wsohmBalance = await wsohmContract.balanceOf(address);
+    } catch (e) {
+      handleContractError(e);
+    }
+    try {
+      const ohmContract = new ethers.Contract(
+        addresses[networkID].OHM_ADDRESS as string,
+        ierc20Abi,
+        provider,
+      ) as IERC20;
+      ohmBalance = await ohmContract.balanceOf(address);
+    } catch (e) {
+      handleContractError(e);
+    }
+    try {
+      const sohmContract = new ethers.Contract(
+        addresses[networkID].SOHM_ADDRESS as string,
+        ierc20Abi,
+        provider,
+      ) as IERC20;
+      sohmBalance = await sohmContract.balanceOf(address);
+    } catch (e) {
+      handleContractError(e);
+    }
+    try {
+      const ohmV2Contract = new ethers.Contract(addresses[networkID].OHM_V2 as string, ierc20Abi, provider) as IERC20;
+      ohmV2Balance = await ohmV2Contract.balanceOf(address);
+    } catch (e) {
+      handleContractError(e);
+    }
+    try {
+      const sohmV2Contract = new ethers.Contract(addresses[networkID].SOHM_V2 as string, ierc20Abi, provider) as IERC20;
+      sohmV2Balance = await sohmV2Contract.balanceOf(address);
+    } catch (e) {
+      handleContractError(e);
+    }
+
+    try {
+      const poolTokenContract = new ethers.Contract(
+        addresses[networkID].PT_TOKEN_ADDRESS as string,
+        ierc20Abi,
+        provider,
+      ) as IERC20;
+      poolBalance = await poolTokenContract.balanceOf(address);
+    } catch (e) {
+      handleContractError(e);
+    }
+    try {
+      for (const fuseAddressKey of ["FUSE_6_SOHM", "FUSE_18_SOHM", "FUSE_36_SOHM"]) {
+        if (addresses[networkID][fuseAddressKey]) {
+          const fsohmContract = new ethers.Contract(
+            addresses[networkID][fuseAddressKey] as string,
+            fuseProxy,
+            provider.getSigner(),
+          ) as FuseProxy;
+          let balanceOfUnderlying = await fsohmContract.callStatic.balanceOfUnderlying(address);
+          const underlying = await fsohmContract.callStatic.underlying();
+          if (underlying == addresses[networkID].GOHM_ADDRESS) {
+            fgohmBalance = balanceOfUnderlying.add(fgohmBalance);
+          } else fsohmBalance = balanceOfUnderlying.add(fsohmBalance);
+        }
+      }
+      const gOhmContract = GOHM__factory.connect(addresses[networkID].GOHM_ADDRESS, provider);
+      if (fgohmBalance.gt(0)) {
+        fgOHMAsfsOHMBalance = await gOhmContract.balanceFrom(fgohmBalance.toString());
+      }
+    } catch (e) {
+      handleContractError(e);
+    }
+    try {
+      if (addresses[networkID].FIATDAO_WSOHM_ADDRESS) {
+        const fiatDaoContract = new ethers.Contract(
+          addresses[networkID].FIATDAO_WSOHM_ADDRESS as string,
+          fiatDAO,
+          provider,
+        ) as FiatDAOContract;
+        fiatDaowsohmBalance = await fiatDaoContract.balanceOf(address, addresses[networkID].WSOHM_ADDRESS as string);
+      }
+    } catch (e) {
+      handleContractError(e);
+    }
+
+    return {
+      balances: {
+        gohm: ethers.utils.formatEther(gOhmBalance),
+        gOhmAsSohmBal: ethers.utils.formatUnits(gOhmBalAsSohmBal, "gwei"),
+        ohmV1: ethers.utils.formatUnits(ohmBalance, "gwei"),
+        sohmV1: ethers.utils.formatUnits(sohmBalance, "gwei"),
+        fsohm: ethers.utils.formatUnits(fsohmBalance, "gwei"),
+        fgohm: ethers.utils.formatEther(fgohmBalance),
+        fgOHMAsfsOHM: ethers.utils.formatUnits(fgOHMAsfsOHMBalance, "gwei"),
+        wsohm: ethers.utils.formatEther(wsohmBalance),
+        fiatDaowsohm: ethers.utils.formatEther(fiatDaowsohmBalance),
+        pool: ethers.utils.formatUnits(poolBalance, "gwei"),
+        ohm: ethers.utils.formatUnits(ohmV2Balance, "gwei"),
+        sohm: ethers.utils.formatUnits(sohmV2Balance, "gwei"),
+      },
+    };
+  },
+);
+
+interface IUserAccountDetails {
+  wrapping: {
+    sohmWrap: number;
+    wsohmUnwrap: number;
+    gOhmUnwrap: number;
+    wsOhmMigrate: number;
+  };
+}
+
+export const getMigrationAllowances = createAsyncThunk(
+  "account/getMigrationAllowances",
+  async ({ networkID, provider, address }: IBaseAddressAsyncThunk) => {
+    let ohmAllowance = BigNumber.from(0);
+    let sOhmAllowance = BigNumber.from(0);
+    let wsOhmAllowance = BigNumber.from(0);
+    let gOhmAllowance = BigNumber.from(0);
+
+    if (addresses[networkID].OHM_ADDRESS) {
+      try {
+        const ohmContract = IERC20__factory.connect(addresses[networkID].OHM_ADDRESS, provider);
+        ohmAllowance = await ohmContract.allowance(address, addresses[networkID].MIGRATOR_ADDRESS);
+      } catch (e) {
+        handleContractError(e);
+      }
+    }
+
+    if (addresses[networkID].SOHM_ADDRESS) {
+      try {
+        const sOhmContract = IERC20__factory.connect(addresses[networkID].SOHM_ADDRESS, provider);
+        sOhmAllowance = await sOhmContract.allowance(address, addresses[networkID].MIGRATOR_ADDRESS);
+      } catch (e) {
+        handleContractError(e);
+      }
+    }
+
+    if (addresses[networkID].WSOHM_ADDRESS) {
+      try {
+        const wsOhmContract = IERC20__factory.connect(addresses[networkID].WSOHM_ADDRESS, provider);
+        wsOhmAllowance = await wsOhmContract.allowance(address, addresses[networkID].MIGRATOR_ADDRESS);
+      } catch (e) {
+        handleContractError(e);
+      }
+    }
+
+    if (addresses[networkID].GOHM_ADDRESS) {
+      try {
+        const gOhmContract = IERC20__factory.connect(addresses[networkID].GOHM_ADDRESS, provider);
+        gOhmAllowance = await gOhmContract.allowance(address, addresses[networkID].MIGRATOR_ADDRESS);
+      } catch (e) {
+        handleContractError(e);
+      }
+    }
+
+    return {
+      migration: {
+        ohm: +ohmAllowance,
+        sohm: +sOhmAllowance,
+        wsohm: +wsOhmAllowance,
+        gohm: +gOhmAllowance,
+      },
+      isMigrationComplete: false,
+    };
+  },
+);
+
+export const loadAccountDetails = createAsyncThunk(
+  "account/loadAccountDetails",
+  async ({ networkID, provider, address }: IBaseAddressAsyncThunk, { dispatch }) => {
+    let stakeAllowance = BigNumber.from("0");
+    let stakeAllowanceV2 = BigNumber.from("0");
+    let unstakeAllowanceV2 = BigNumber.from("0");
+    let unstakeAllowance = BigNumber.from("0");
+    let wrapAllowance = BigNumber.from("0");
+    let gOhmUnwrapAllowance = BigNumber.from("0");
+    let poolAllowance = BigNumber.from("0");
+    let ohmToGohmAllowance = BigNumber.from("0");
+    let wsOhmMigrateAllowance = BigNumber.from("0");
+
+    try {
+      const gOhmContract = GOHM__factory.connect(addresses[networkID].GOHM_ADDRESS, provider);
+      gOhmUnwrapAllowance = await gOhmContract.allowance(address, addresses[networkID].STAKING_V2);
+
+      const wsOhmContract = IERC20__factory.connect(addresses[networkID].WSOHM_ADDRESS, provider);
+      wsOhmMigrateAllowance = await wsOhmContract.balanceOf(address);
+
+      const ohmContract = new ethers.Contract(
+        addresses[networkID].OHM_ADDRESS as string,
+        ierc20Abi,
+        provider,
+      ) as IERC20;
+      stakeAllowance = await ohmContract.allowance(address, addresses[networkID].STAKING_HELPER_ADDRESS);
+
+      const sohmContract = new ethers.Contract(addresses[networkID].SOHM_V2 as string, sOHMv2, provider) as SOhmv2;
+      unstakeAllowance = await sohmContract.allowance(address, addresses[networkID].STAKING_ADDRESS);
+      poolAllowance = await sohmContract.allowance(address, addresses[networkID].PT_PRIZE_POOL_ADDRESS);
+      wrapAllowance = await sohmContract.allowance(address, addresses[networkID].STAKING_V2);
+
+      const sohmV2Contract = IERC20__factory.connect(addresses[networkID].SOHM_V2, provider);
+      unstakeAllowanceV2 = await sohmV2Contract.allowance(address, addresses[networkID].STAKING_V2);
+
+      const ohmV2Contract = IERC20__factory.connect(addresses[networkID].OHM_V2, provider);
+      stakeAllowanceV2 = await ohmV2Contract.allowance(address, addresses[networkID].STAKING_V2);
+    } catch (e) {
+      console.warn("failed contract calls in slice", e);
+    }
+    await dispatch(getBalances({ address, networkID, provider }));
+
+    return {
+      staking: {
+        ohmStakeV1: +stakeAllowance,
+        ohmUnstakeV1: +unstakeAllowance,
+        ohmStake: +stakeAllowanceV2,
+        ohmUnstake: +unstakeAllowanceV2,
+        ohmtoGohm: +ohmToGohmAllowance,
+      },
+      wrapping: {
+        sohmWrap: Number(ethers.utils.formatUnits(wrapAllowance, "gwei")),
+        gOhmUnwrap: Number(ethers.utils.formatUnits(gOhmUnwrapAllowance, "ether")),
+        wsOhmMigrate: Number(ethers.utils.formatUnits(wsOhmMigrateAllowance, "ether")),
+      },
+    };
+  },
+);
+
+export interface IUserBondDetails {
+  // bond: string;
+  allowance: number;
+  interestDue: number;
+  bondMaturationBlock: number;
+  pendingPayout: string; //Payout formatted in gwei.
+}
+export const calculateUserBondDetails = createAsyncThunk(
+  "account/calculateUserBondDetails",
+  async ({ address, bond, networkID, provider }: ICalcUserBondDetailsAsyncThunk) => {
+    if (!address) {
+      return {
+        bond: "",
+        displayName: "",
+        bondIconSvg: "",
+        isLP: false,
+        allowance: 0,
+        balance: "0",
+        interestDue: 0,
+        bondMaturationBlock: 0,
+        pendingPayout: "",
+      };
+    }
+    // dispatch(fetchBondInProgress());
+
+    // Calculate bond details.
+    const bondContract = bond.getContractForBond(networkID, provider);
+    const reserveContract = bond.getContractForReserve(networkID, provider);
+
+    let pendingPayout, bondMaturationBlock;
+
+    const bondDetails = await bondContract.bondInfo(address);
+    let interestDue: BigNumberish = Number(bondDetails.payout.toString()) / Math.pow(10, 9);
+    bondMaturationBlock = +bondDetails.vesting + +bondDetails.lastBlock;
+    pendingPayout = await bondContract.pendingPayoutFor(address);
+
+    let allowance,
+      balance = BigNumber.from(0);
+    allowance = await reserveContract.allowance(address, bond.getAddressForBond(networkID) || "");
+    balance = await reserveContract.balanceOf(address);
+    // formatEthers takes BigNumber => String
+    const balanceVal = ethers.utils.formatEther(balance);
+    // balanceVal should NOT be converted to a number. it loses decimal precision
+    return {
+      bond: bond.name,
+      displayName: bond.displayName,
+      bondIconSvg: bond.bondIconSvg,
+      isLP: bond.isLP,
+      allowance: Number(allowance.toString()),
+      balance: balanceVal,
+      interestDue,
+      bondMaturationBlock,
+      pendingPayout: ethers.utils.formatUnits(pendingPayout, "gwei"),
+    };
+  },
+);
+
+interface IAccountSlice extends IUserAccountDetails {
+  bonds: { [key: string]: IUserBondDetails };
+  balances: {
+    gohm: string;
+    gOhmAsSohmBal: string;
+    ohmV1: string;
+    ohm: string;
+    sohm: string;
+    sohmV1: string;
+    dai: string;
+    oldsohm: string;
+    fsohm: string;
+    fgohm: string;
+    fgOHMAsfsOHM: string;
+    wsohm: string;
+    fiatDaowsohm: string;
+    pool: string;
+  };
+  loading: boolean;
+  staking: {
+    ohmStakeV1: number;
+    ohmUnstakeV1: number;
+    ohmStake: number;
+    ohmUnstake: number;
+  };
+  migration: {
+    ohm: number;
+    sohm: number;
+    wsohm: number;
+    gohm: number;
+  };
+  pooling: {
+    sohmPool: number;
+  };
+  isMigrationComplete: boolean;
+}
+
+const initialState: IAccountSlice = {
+  loading: false,
+  bonds: {},
+  balances: {
+    gohm: "",
+    gOhmAsSohmBal: "",
+    ohmV1: "",
+    ohm: "",
+    sohm: "",
+    sohmV1: "",
+    dai: "",
+    oldsohm: "",
+    fsohm: "",
+    fgohm: "",
+    fgOHMAsfsOHM: "",
+    wsohm: "",
+    fiatDaowsohm: "",
+    pool: "",
+  },
+  staking: { ohmStakeV1: 0, ohmUnstakeV1: 0, ohmStake: 0, ohmUnstake: 0 },
+  wrapping: { sohmWrap: 0, wsohmUnwrap: 0, gOhmUnwrap: 0, wsOhmMigrate: 0 },
+  pooling: { sohmPool: 0 },
+  migration: { ohm: 0, sohm: 0, wsohm: 0, gohm: 0 },
+  isMigrationComplete: false,
+};
+
+const accountSlice = createSlice({
+  name: "account",
+  initialState,
+  reducers: {
+    fetchAccountSuccess(state, action) {
+      setAll(state, action.payload);
+    },
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(loadAccountDetails.pending, state => {
+        state.loading = true;
+      })
+      .addCase(loadAccountDetails.fulfilled, (state, action) => {
+        setAll(state, action.payload);
+        state.loading = false;
+      })
+      .addCase(loadAccountDetails.rejected, (state, { error }) => {
+        state.loading = false;
+        console.log(error);
+      })
+      .addCase(getBalances.pending, state => {
+        state.loading = true;
+      })
+      .addCase(getBalances.fulfilled, (state, action) => {
+        setAll(state, action.payload);
+        state.loading = false;
+      })
+      .addCase(getBalances.rejected, (state, { error }) => {
+        state.loading = false;
+        console.log(error);
+      })
+      .addCase(calculateUserBondDetails.pending, state => {
+        state.loading = true;
+      })
+      .addCase(calculateUserBondDetails.fulfilled, (state, action) => {
+        if (!action.payload) return;
+        const bond = action.payload.bond;
+        state.bonds[bond] = action.payload;
+        state.loading = false;
+      })
+      .addCase(calculateUserBondDetails.rejected, (state, { error }) => {
+        state.loading = false;
+        console.log(error);
+      })
+      .addCase(getMigrationAllowances.fulfilled, (state, action) => {
+        setAll(state, action.payload);
+      })
+      .addCase(getMigrationAllowances.rejected, (state, { error }) => {
+        console.log(error);
+      });
+  },
+});
+
+export default accountSlice.reducer;
+
+export const { fetchAccountSuccess } = accountSlice.actions;
+
+const baseInfo = (state: RootState) => state.account;
+
+export const getAccountState = createSelector(baseInfo, account => account);
